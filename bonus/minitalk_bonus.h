75c2a551983743b210a26f33631fc316e4401c8e
Cheng Soo Tan <chtan@u82z01s02.42kl.edu.my>
2024-07-14 16:22:43 +0800

    add files

--- bonus/minitalk_bonus.h
+++ bonus/minitalk_bonus.h
@@ -6,21 +6,21 @@
 /*   By: chtan <chtan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 13:34:42 by chtan             #+#    #+#             */
-/*   Updated: 2024/07/13 13:38:32 by chtan            ###   ########.fr       */
+/*   Updated: 2024/07/14 09:28:02 by chtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_BONUS_H
 # define MINITALK_BONUS_H
-#include <signal.h>
-#include "../libft/libft.h"
+# include <signal.h>
+# include "../libft/libft.h"
 
 // server
 void	config_signals(void);
 void	handle_sigusr(int signum, siginfo_t *info, void *ucontent);
 
 // client
-void send_signal(int pid, char *str);
-void message(int num);
+void	send_signal(int pid, char *str);
+void	message(int num);
 
-# endif
+#endif
